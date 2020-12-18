69c02bf38988f2a244e2fb0dee3df3fb99078411
Cindy Qian <cindyqian.yushi@gmail.com>
2020-12-18 11:15:36 -0800

    before adding login page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  // document.body.style="background-image: linear-gradient(to right, #4D446F, #Df825F)";
+  document.body.style.backgroundImage="linear-gradient(to right, #51A8C2, #B6F7DA)";
   return (
-    <div>chameleon</div>
+    <div className="Body">
+      <div classname="NavBar">
+        <div className="Header">Chameleon</div>
+      </div>
+
+
+      <div className="LeftColumn">
+        <div classname="profile">
+        
+        </div>
+        <div classname="calendarOptions">
+
+        </div>
+      </div>
+
+
+      <div className="RightColumn">
+        <div classname="calendar">
+          
+        </div>
+      </div>
+      
+      
+    </div>
+
   );
 }
 
---
+++ src/components/CalendarCell.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './CalendarCell.css';
+
+function CalendarCell() {
+    return <svg className = "cell"> 
+        <rect rx = "15" ry = "15" width = "125" height = "125" />
+    </svg>
+}
