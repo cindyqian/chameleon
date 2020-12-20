abf05db67f285b0628ce953ab2c3adc069d29795
Melody Zhu <melodyyzhu@gmail.com>
2020-12-19 19:56:29 -0800

    searchbar2 test again

---
+++ src/components/SearchBar2.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./SearchBar2.css";
+
+const SearchBar = ({ keyword, setKeyword }) => {
+  const BarStyling = {
+    backgroundColor: "#white",
+    width: "100%",
+    padding: "20px",
+    borderRadius: "30px",
+    color: "#6FAEA3",
+    opacity: 0.7,
+    fontSize: "18px",
+    boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.301)",
+    border: "none"
+  };
+
+  return (
+    <div>
+      iuhrksjefdehkusjd
+      {/* <input /> */}
+    </div>
+  );
+};
+
+export default SearchBar;
